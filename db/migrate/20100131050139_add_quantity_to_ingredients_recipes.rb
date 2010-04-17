f0df0d8e97c460502c46e5a5345cce9c7b7ccda2
Kyle Powers <kylepowers@gmail.com>
2010-04-17 01:25:46 -0500

    updating database for rails3

--- db/migrate/20100131050139_add_quantity_to_ingredients_recipes.rb
+++
@@ -1,9 +0,0 @@
-class AddQuantityToIngredientsRecipes < ActiveRecord::Migration
-  def self.up
-  	add_column :ingredients_recipes, :quantity, :string
-  end
-
-  def self.down
-  	remove_column :ingredients_recipes, :quantity
-  end
-end
