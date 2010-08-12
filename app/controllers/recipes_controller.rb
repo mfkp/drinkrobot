1e2019c42c44652eb58600df50260e9dc26ff064
Kyle Powers <kylepowers@gmail.com>
2010-08-12 13:23:16 -0500

    testing sql

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -52,10 +52,10 @@ class RecipesController < ApplicationController
 			  WHERE ingredient_id IN(" + ingreds + ") GROUP BY recipe_id  
 			  HAVING Number <= " + @ingredients.length.to_s
 	possiblerecipes = IngredientsRecipesQuantity.find_by_sql(sqlStr)
-#	
-#	possiblerecipes.each do |recipe|
-#		recipes.push(Recipe.find(recipe.recipe_id))
-#	end
+	
+	possiblerecipes.each do |recipe|
+		recipes.push(Recipe.find_by_id(recipe.recipe_id))
+	end
 
   	recipes.each do |recipe|
 		if @ingredients.include? recipe.ingredients.first.id.to_s
