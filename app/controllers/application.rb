9593b67096a020d7cebbda9c9bd72f480778a2ba
Kyle Powers <kylepowers@gmail.com>
2010-09-06 17:30:35 -0500

    adding mobile browser detection

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,7 +1,8 @@
 # Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
-
+require 'is_it_mobile' 
 class ApplicationController < ActionController::Base
+  include IsItMobile::ForRails
   helper :all # include all helpers, all the time
 
   # See ActionController::RequestForgeryProtection for details
