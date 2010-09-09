6637e2fbaefd055f12c68e4242a6d004c1e1f838
Kyle Powers <kylepowers@gmail.com>
2010-09-09 12:12:07 -0500

    testing app controller

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,16 +1,15 @@
 # Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
-class ApplicationController < ActionController::Base
-  has_mobile_fu(true)
-  helper :all # include all helpers, all the time
+#class ApplicationController < ActionController::Base
+  #has_mobile_fu(true)
+#  helper :all # include all helpers, all the time
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => '94d87aca9f106476e4b19a3b24b7bdea'
+#  protect_from_forgery # :secret => '94d87aca9f106476e4b19a3b24b7bdea'
   
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
   # filter_parameter_logging :password
-  layout 'application'
-end
+#end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
-#class ApplicationController < ActionController::Base
-#  has_mobile_fu
-#  protect_from_forgery
-#  layout 'application'
-#end
+class ApplicationController < ActionController::Base
+  has_mobile_fu(true)
+  protect_from_forgery
+  layout 'application'
+end
