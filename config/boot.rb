fac87dbaad30244c6b8c7d976d2826a1b11305c3
Kyle Powers <kylepowers@gmail.com>
2010-10-10 23:30:26 -0500

    fixing dynamic_form plugin

--- config/boot.rb
+++ config/boot.rb
@@ -1,21 +1,8 @@
-#require 'rubygems'
-#if File.exist?(File.expand_path('../../Gemfile', __FILE__))
-#  require 'bundler'
-#  Bundler.setup
-#end
-
-# This configuration will work on heroku (16.04.2010)
-# congig/boot.rb (rails3 app) or config/preinitializer.rb (rails 2.3.5 app)
-
-# Use locked gems if present.
 begin
   require File.expand_path('../../.bundle/environment', __FILE__)
 
 rescue LoadError
-  # Otherwise, use RubyGems.
   require 'rubygems'
-
-  # And set up the gems listed in the Gemfile.
   if File.exist?(File.expand_path('../../Gemfile', __FILE__))
     require 'bundler'
     Bundler.setup
