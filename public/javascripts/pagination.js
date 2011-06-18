7bcafa3fb8a999b30328848719d61983266a58c4
Kyle Powers <kylepowers@gmail.com>
2011-06-17 21:47:09 -0700

    updating paganation to include pushstate in supported browsers

--- public/javascripts/pagination.js
+++ public/javascripts/pagination.js
@@ -1,6 +1,8 @@
 $(function() {
   $(".pagination a").live("click", function() {
     $.get(this.href, null, null, "script");
+    var arr = this.href.split('/');
+    window.history.replaceState(null, 'Drink Recipes', arr[arr.length-1]);
     return false;
   });
 });
